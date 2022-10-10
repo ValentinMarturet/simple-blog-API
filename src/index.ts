163e07f7e7f2078a067d99a166e7cff5e82b67b3
unknown <valen.martu2206@gmail.com>
2022-10-10 16:51:43 -0300

    agregando rutas

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,59 @@ app.get("/users", async (req: Request, res: Response) => {
   res.json(users);
 });
 
+app.get("/feed", async (req: Request, res: Response) => {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  });
+  res.json(posts);
+});
+
+app.get("/post/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const post = await prisma.post.findFirst({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
+
+app.post("/user", async (req: Request, res: Response) => {
+  const result = await prisma.user.create({
+    data: { ...req.body },
+  });
+  res.json(result);
+});
+
+app.post("/post", async (req: Request, res: Response) => {
+  const { title, content, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: false,
+      author: { connect: { email: authorEmail } },
+    },
+  });
+  res.json(result);
+});
+
+app.put("/post/publish/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const post = await prisma.post.update({
+    where: { id: Number(id) },
+    data: { published: true },
+  });
+  res.json(post);
+});
+
+app.delete("/post/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const post = await prisma.post.delete({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
+
 app.listen(3000, () =>
   console.log("REST API server ready at: http://localhost:3000")
 );
